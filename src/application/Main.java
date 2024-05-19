4ae660a13d1e6392ea41f8245fad8a597507b64d
damirmuminovic <damir.muminovic@fet.ba>
2024-05-19 18:03:04 +0200

    Add files via upload

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,54 @@
-package application;
+package projekat;
+import java.sql.*;
+import javax.sql.*;
+
+
+public class Main {
 	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
-
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
+	
+	
+	
+	
+	public static void main(String[]args) 
+	{
+		
+		String url="jdbc:mysql://localhost:3306/rsprojekat";
+		String username="root";
+		//////////////////////////////////////////	//////////////////////////////////////////
+		String password=" "; // UNIJETI password	
+		//////////////////////////////////////////	//////////////////////////////////////////
+		
+		User user = new User(1002, "Damir", "Muminovic", "damirmuminovic@fet.ba", "123456", 500, 1, 1, Date.valueOf("2023-05-19"), 1, "2023-05-19");
+     
+		
+		user.createUser(user); 
+		
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		Connection con=DriverManager.getConnection(url,username,password);
+		
+		Statement statement=con.createStatement();
+		
+		ResultSet resultSet=statement.executeQuery("SELECT *FROM korisnik");
+		
+		
+		//ispis podataka o korisniku
+		while (resultSet.next()) 
+		{
+			System.out.print("ID:"+resultSet.getString(1)+", Ime: "+resultSet.getString(2)+", Prezime: "+resultSet.getString(3));
+			
 		}
+				}
+		catch(Exception e)
+		{
+			System.out.println(e);
+			
+		}
+		
+
+		
+		
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
 }
