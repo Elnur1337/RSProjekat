98c4a76a65b38b00aa1138b9b9d06ea8d4859690
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2024-05-20 14:59:00 +0200

    Added fxml objects to register controller

--- src/application/RegisterController.java
+++ src/application/RegisterController.java
@@ -9,15 +9,44 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class RegisterController {
+	private String errorMsg;
+	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
 	@FXML
 	private Button homeBtn;
+	@FXML
+	private TextField imeInput;
+	@FXML
+	private TextField prezimeInput;
+	@FXML
+	private TextField emailInput;
+	@FXML
+	private PasswordField passwordInput;
+	@FXML
+	private PasswordField passwordRepeatInput;
+	@FXML
+	private ChoiceBox<Integer> yearInput;
+	@FXML
+	private ChoiceBox<Integer> monthInput;
+	@FXML
+	private ChoiceBox<Integer> dayInput;
+	@FXML
+	private CheckBox isOrganizatorInput;
+	@FXML
+	private Button registerBtn;
+	@FXML
+	private Label errorMsgLabel;
 	
 	public void switchToHomeScene(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("index.fxml"));
@@ -27,4 +56,6 @@ public class RegisterController {
 		stage.show();
 		return;
 	}
+	
+	private bool validate()
 }
