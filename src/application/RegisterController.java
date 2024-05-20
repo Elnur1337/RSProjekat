8046d3d1339a917f0c4eb77e33d83ec90599ee4f
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2024-05-20 21:27:23 +0200

    Added initialize method and created options for choicebox in register form

--- src/application/RegisterController.java
+++ src/application/RegisterController.java
@@ -1,24 +1,35 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.regex.Pattern;
+import java.util.ResourceBundle;
 import java.util.regex.Matcher;
+import java.util.HashMap;
+import java.util.Map;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class RegisterController {
+public class RegisterController implements Initializable {
 	private String errorMsg;
+	private Map<String, Integer> monthNameNumberMap = new HashMap<>();
+	private Map<String, Integer> monthNameDaysMap = new HashMap<>();
 	
 	private Stage stage;
 	private Scene scene;
@@ -39,7 +50,7 @@ public class RegisterController {
 	@FXML
 	private ChoiceBox<Integer> yearInput;
 	@FXML
-	private ChoiceBox<Integer> monthInput;
+	private ChoiceBox<String> monthInput;
 	@FXML
 	private ChoiceBox<Integer> dayInput;
 	@FXML
@@ -55,6 +66,68 @@ public class RegisterController {
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
+		return;
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		monthNameNumberMap.put("Januar", 1);
+		monthNameNumberMap.put("Februar", 2);
+		monthNameNumberMap.put("Mart", 3);
+		monthNameNumberMap.put("April", 4);
+		monthNameNumberMap.put("Maj", 5);
+		monthNameNumberMap.put("Jun", 6);
+		monthNameNumberMap.put("Jul", 7);
+		monthNameNumberMap.put("August", 8);
+		monthNameNumberMap.put("Septembar", 9);
+		monthNameNumberMap.put("Oktobar", 10);
+		monthNameNumberMap.put("Novembar", 11);
+		monthNameNumberMap.put("Decembar", 12);
+		
+		monthNameDaysMap.put("Januar", 31);
+		monthNameDaysMap.put("Februar", 29);
+		monthNameDaysMap.put("Mart", 31);
+		monthNameDaysMap.put("April", 30);
+		monthNameDaysMap.put("Maj", 31);
+		monthNameDaysMap.put("Jun", 30);
+		monthNameDaysMap.put("Jul", 31);
+		monthNameDaysMap.put("August", 31);
+		monthNameDaysMap.put("Septembar", 30);
+		monthNameDaysMap.put("Oktobar", 31);
+		monthNameDaysMap.put("Novembar", 30);
+		monthNameDaysMap.put("Decembar", 31);
+		
+		for (int i = 1970; i < 2025; ++i) {
+			yearInput.getItems().add(i);
+		}
+		
+		//Zamijeniti choicebox sa combobox jer podrzava scroll
+		//Dodati iteme u combobox za mjesec u jednoj liniji
+		
+		monthInput.getItems().add("Januar");
+		monthInput.getItems().add("Februar");
+		monthInput.getItems().add("Mart");
+		monthInput.getItems().add("April");
+		monthInput.getItems().add("Maj");
+		monthInput.getItems().add("Jun");
+		monthInput.getItems().add("Jul");
+		monthInput.getItems().add("August");
+		monthInput.getItems().add("Septembar");
+		monthInput.getItems().add("Oktobar");
+		monthInput.getItems().add("Novembar");
+		monthInput.getItems().add("Decembar");
+		
+		monthInput.getSelectionModel().selectedItemProperty().addListener(
+				new ChangeListener<String>() {
+					@Override
+					public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+						dayInput.getItems().clear();
+						for (int i = 1; i <= monthNameDaysMap.get(newValue); ++i) {
+							dayInput.getItems().add(i);
+						}
+					}
+				}
+		);
 		return;
 	}
 	
@@ -100,8 +173,10 @@ public class RegisterController {
 	
 	public void register() {
 		if (validate()) {
+			
 			//Registruj korisnika u bazu
 		}
 		return;
 	}
+
 }
