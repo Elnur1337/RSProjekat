53590c14c7d1687c4fe32de8d1c03c79824d6656
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2024-05-20 12:17:04 +0200

    Added navbar to register scene

--- src/application/RegisterController.java
+++ src/application/RegisterController.java
@@ -1,5 +1,30 @@
 package application;
 
-public class RegisterController {
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class RegisterController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	@FXML
+	private Button homeBtn;
+	
+	public void switchToHomeScene(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("index.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		return;
+	}
 }
