68093d57ba71d2191653ebfea7c6c6c0691fdd50
damirmuminovic <damir.muminovic@fet.ba>
2024-06-10 13:50:32 +0200

    Add files via upload

---
+++ src/application/Karta.java
@@ -0,0 +1,24 @@
+package projekat;
+
+public class Karta {
+
+	
+	private int sifKarta_;
+	private double cijena_;
+	private String period_;
+	private String uslovOtkz_;
+	
+	
+	Karta(){}
+	
+	Karta(int sifKarta,double cijena,String period,String uslovOtkz)
+	{
+		sifKarta_=sifKarta;
+		cijena_=cijena;
+		period_=period;
+		uslovOtkz_=uslovOtkz;
+		
+		
+	}
+	
+}
---
+++ src/application/Sjedalo.java
@@ -0,0 +1,33 @@
+package projekat;
+
+public class Sjedalo {
+
+	private int id_;
+	private int idSektor_;
+	
+	Sjedalo(){};
+	
+	public int getId_() {
+		return id_;
+	}
+
+
+	public void setId_(int id_) {
+		this.id_ = id_;
+	}
+
+
+	public int getIdSektor_() {
+		return idSektor_;
+	}
+
+
+	public void setIdSektor_(int idSektor_) {
+		this.idSektor_ = idSektor_;
+	}
+
+
+	
+	
+	
+}
