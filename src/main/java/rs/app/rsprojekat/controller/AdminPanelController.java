85a4410c4c1962f44fd6f5d9a4a3b6d0293f83b7
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2024-07-03 11:33:28 +0200

    Testing pagination page factory

--- src/main/java/rs/app/rsprojekat/controller/AdminPanelController.java
+++ src/main/java/rs/app/rsprojekat/controller/AdminPanelController.java
@@ -8,19 +8,30 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import rs.app.rsprojekat.model.User;
 
+import javax.persistence.*;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class AdminPanelController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    private List<User> userList;
+
+    @FXML
+    private Pagination usersPagination;
 
     public void switchToHomeScene(ActionEvent event) throws IOException {
         final URL url = Paths.get("src/main/resources/rs/app/rsprojekat/index.fxml").toUri().toURL();
@@ -46,12 +57,46 @@ public class AdminPanelController implements Initializable {
         stage.show();
     }
 
-    public void test() {
-        System.out.println("test");
+    public void showUserPanel() {
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("rsprojekat");
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u WHERE approved = 0", User.class);
+        try  {
+            userList = query.getResultList();
+        } catch (NoResultException ignored) {}
+        for (User u : userList) {
+            System.out.println(u.getEmail());
+        }
     }
+
+
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        //Nije potrebno?
+        usersPagination.setPageFactory(this::userTest);
+    }
+
+    VBox userTest(int pageIndex) {
+        //ovo prebaciti u callback
+        //kreirati lijep dizajn boxova sa svim informacijama
+        //dodati mogucnost prihvatanja i odbijanja i refresh na svako odbijanje
+        //istu funkcionalnost dodati i na klik na menu
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("rsprojekat");
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u WHERE approved = 0", User.class).setFirstResult(pageIndex * 5).setMaxResults(5);
+        try  {
+            userList = query.getResultList();
+        } catch (NoResultException ignored) {}
+
+        VBox page = new VBox();
+        for (User u : userList) {
+            HBox userBox = new HBox();
+            Label firstName = new Label(u.getIme());
+            Label email = new Label(u.getEmail());
+            userBox.getChildren().add(email);
+            page.getChildren().add(userBox);
+            System.out.println(email.getText());
+        }
+        return page;
     }
 }
